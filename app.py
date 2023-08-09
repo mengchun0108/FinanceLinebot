6b78b5c3110ee53f8fc6a394ce46a2711bf83ed2
mengchun0108 <139243811+mengchun0108@users.noreply.github.com>
2023-08-09 10:45:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def handle_message(event):
         # 用push_message方式回覆話語
 
     # 股價查詢
-    if re.match("想知道股價[0-9]:", msg):
+    if re.match("想知道股價:", msg):
         stockNumber = msg[2:6]
         btn_msg = stock_reply_other(stockNumber)
         line_bot_api.push_message(uid, btn_msg)
