2b454af33effc456513c67a345cdb79a0879b1ab
mengchun0108 <139243811+mengchun0108@users.noreply.github.com>
2023-08-09 13:34:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from line_bot_api import *
 from events.basic import *
 from events.oil import *
 from events.Msg_Template import *
+from model.mongodb import *
 import re
 import twstock
 import datetime
@@ -41,6 +42,7 @@ def handle_message(event):
     msg = str(event.message.text).upper().strip()
     # 使用者輸入的內容
     emsg = event.message.text
+    user_name = profile.display_name #使用者名稱
 
     ############################## 使用說明 選單 基德 油價查詢 ##############################
     if message_text == '@使用說明':
@@ -88,6 +90,23 @@ def handle_message(event):
     if re.match("想知道股價", msg):
         btn_msg = stock_reply_other(msg)
         line_bot_api.push_message(uid, btn_msg)
+        return 0
+    
+    if re.match("想知道股價[0-9]", msg):
+        msg = msg[5:]
+        btn_msg = stock_reply_other(msg)
+        line_bot_api.push_message(uid, btn_msg)
+        return 0
+    
+    # 新增使用者關注的股票到mongodb
+    if re.match('關注[0-9]{4}[<>][0-9]', msg):
+        # 使用者新增股票質股票清單
+        stockNumber = msg[2:6]
+        content = write_my_stock(uid, user_name, stockNumber, msg[6:7], msg[7:])
+        line_bot_api.push_message(uid, TextSendMessage(content))
+    else:
+        content = write_my_stock(uid, user_name, "未設定", "未設定")
+        line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
     
     if (emsg.startswith('#')):
