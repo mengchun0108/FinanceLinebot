35b63ed3a48a0b620b02b57f5e5bdffba8123387
mengchun0108 <139243811+mengchun0108@users.noreply.github.com>
2023-08-10 10:09:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -148,10 +148,10 @@ def handle_message(event):
         message = show_Button()
         line_bot_api.reply_message(event.reply_token, message)
 
-    if re.match('換匯[A-Z]{3}/[A-Z]{3}/100',emsg):
-        line_bot_api.reply_message(uid, TextSendMessage("基德將為您做外匯計算"))
+    if re.match('換匯[A-Z]{3}/[A-Z]{3}/100',msg):
+        line_bot_api.push_message(uid, TextSendMessage("基德將為您做外匯計算"))
         content = getExchangeRate(msg)
-        line_bot_api.reply_message(uid, TextSendMessage(content))
+        line_bot_api.push_message(uid, TextSendMessage(content))
                                               
 @handler.add(FollowEvent)
 def handle_follow(event):
