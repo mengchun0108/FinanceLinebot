1cbd2481d5baed259ae4b6acf7368a9199122577
mengchun0108 <139243811+mengchun0108@users.noreply.github.com>
2023-12-09 23:14:40 +0800

    Add files via upload

--- line_bot_api.py
+++ line_bot_api.py
@@ -7,6 +7,6 @@ from linebot.models import *
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('y183jnCciIWryNOI+kTjMm80wyo/KStYQCOLMlqrz4UZ62jOrdkaKMZ/N51MWbMfeqPB6pLdVbTxBim+pn6HExanDVsx7N994f0uOPVrVE/iBJiwBCWexTrbmIFrf5P3CG8LbKBseyKInUlkvynGgwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('RAwLYo3oZVIR84NQ/a//R0FFmtzS4kkuty8tL7fkAsc6hEjUk48q0JtbpPlX8mr9cDyYZk0NvTToTIkhx6mRbVyeT7s/dvNl1vKKxmNhX3pmiKwFqlx8q0H51r4Wzri0ZWgWQaBaKi0Z0wvOqOw0kQdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('b0d867b52ea2085d294fbf521e2119d6')
+handler = WebhookHandler('930c96dd10e22be503722d4d486a507d')
